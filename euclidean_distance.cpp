002f56717940bfc7dbb861fc050cfbdc3ddbefd0
scottberry <scottdberry@gmail.com>
2019-04-04 12:17:06 +0200

    Changed the implementation of euclidean distance metric to C for performance

---
+++ euclidean_distance.cpp
@@ -0,0 +1,11 @@
+#include <pybind11/pybind11.h>
+#include <math.h>
+
+double metric(double x1, double y1, double x2, double y2) {
+    return sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+}
+
+PYBIND11_MODULE(euclidean_distance, m) {
+    m.doc() = "euclidean distance metric";
+    m.def("metric", &metric, "A function which returns the euclidean distance from (x1,y1) to (x2,y2)");
+}
